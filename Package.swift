044a28347b3db87dd0a26201f116500d22cc4eef
xxz <hcheo@sb-solutions.co.kr>
2024-10-22 18:18:44 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SoomirangSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // SDK를 라이브러리로 정의
+        .library(
+            name: "SoomirangSDK",
+            targets: ["SoomirangSDK"]),
+    ],
+    dependencies: [
+        // 여기에 외부 의존성이 있으면 추가합니다.
+    ],
+    targets: [
+        // SDK 코드가 포함된 타겟
+        .target(
+            name: "SoomirangSDK",
+            dependencies: []),
+        // 테스트 코드가 포함된 타겟
+        .testTarget(
+            name: "SoomirangSDKTests",
+            dependencies: ["SoomirangSDK"]),
+    ]
+)
